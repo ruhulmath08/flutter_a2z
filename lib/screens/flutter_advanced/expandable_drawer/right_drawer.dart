65236b5a0563c7381ae113cf97a20d946894d0d8
ruhulmath08 <ruhulmath08@gmail.com>
2021-11-28 01:04:44 +0600

    delete new file.

--- lib/screens/flutter_advanced/expandable_drawer/right_drawer.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Widget rightDrawer(BuildContext context){
-  return Drawer(
-    child: ListView(
-      physics: const BouncingScrollPhysics(),
-    ),
-  );
-}
