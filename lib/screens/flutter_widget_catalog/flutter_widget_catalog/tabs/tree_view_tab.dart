cf14f017cf9009f1dd9521f0e4e7928f6919e1c7
ruhulmath08 <ruhulmath08@gmail.com>
2022-02-26 01:35:33 +0600

    add flutter_widget_catalog tree view

--- lib/screens/flutter_widget_catalog/flutter_widget_catalog/tabs/tree_view_tab.dart
+++ lib/screens/flutter_widget_catalog/flutter_widget_catalog/tabs/tree_view_tab.dart
@@ -1,34 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_a2z/models/flutter_widget_catalog/flutter_widget_catalog_model_details.dart';
-import 'package:tree_view/tree_view.dart';
 
-class TreeViewTab extends StatelessWidget {
+class TreeViewTab extends StatefulWidget {
   const TreeViewTab({Key? key}) : super(key: key);
+
+  @override
+  State<TreeViewTab> createState() => _TreeViewTabState();
+}
+
+class _TreeViewTabState extends State<TreeViewTab> {
+  bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     final List<FlutterWidgetCatalogModelDetails> models = FlutterWidgetCatalogModelDetails.fetchAll();
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TreeView(
-        startExpanded: true,
-        children: models
-            .map(
-              (e) => TreeViewChild(
-                parent: Card(
-                  child: ListTile(
-                    leading: Text(e.name),
-                  ),
-                ),
-                children: e.subWidget
-                    .map(
-                      (e) => Text(e.name),
-                    )
-                    .toList(),
+    return ListView(
+      children: models
+          .map(
+            (e) => Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: boxDecoration(context),
+              child: ExpansionTile(
+                title: Text(e.name),
+                children: e.subWidget.map((e) {
+                  if (e.subWidget.isEmpty) {
+                    return ListTile(
+                      title: Text(e.name),
+                    );
+                  } else {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: boxDecoration(context),
+                      child: ExpansionTile(
+                        title: Text(e.name),
+                        children: e.subWidget.map((e) {
+                          if (e.subWidget.isEmpty) {
+                            return ListTile(title: Text(e.name));
+                          } else {
+                            return Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                              decoration: boxDecoration(context),
+                              child: ExpansionTile(
+                                title: Text(e.name),
+                                children: e.subWidget.map((e) {
+                                  if (e.subWidget.isEmpty) {
+                                    return ListTile(title: Text(e.name));
+                                  } else {
+                                    return ExpansionTile(title: Text(e.name));
+                                  }
+                                }).toList(),
+                              ),
+                            );
+                          }
+                        }).toList(),
+                      ),
+                    );
+                  }
+                }).toList(),
               ),
-            )
-            .toList(),
-      ),
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  BoxDecoration boxDecoration(BuildContext context) {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: const BorderRadius.all(Radius.circular(10)),
+      border: Border.all(color: Theme.of(context).primaryColor),
     );
   }
 }
